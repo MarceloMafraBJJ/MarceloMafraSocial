44b21bb9b665b0cc451cdf990ac29028d0a3d8ff
marcelomafrabjj <marcelo.mafrademoura@gmail.com>
2022-07-24 17:24:30 -0300

    first commit

---
+++ items.js
@@ -0,0 +1,26 @@
+const hamburguersJson = [
+  {
+    id: 1,
+    name: "Instagram",
+    img: "./src/assets/hamburguer1.jpeg",
+  },
+  {
+    id: 2,
+    name: " Twitter",
+    img: "./src/assets/hamburguer1.jpeg",
+  },
+  {
+    id: 3,
+    name: "GitHub",
+    img: "./src/assets/hamburguer1.jpeg",
+  },
+  {
+    id: 4,
+    name: "LinkedIn",
+    img: "./src/assets/hamburguer1.jpeg",
+  },
+ 
+ 
+];
+
+export default hamburguersJson;
